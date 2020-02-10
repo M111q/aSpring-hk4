12832e20a55db995e2b416f3f053f075989505b9
M111q <miq1991@gmail.com>
2020-02-10 11:03:21 +0100

    script.js done

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,5 @@
 const API_URL = 'http://localhost:8080/cars';
+const carsDiv = document.getElementById('cars');
 
 getAllCars();
 
@@ -7,63 +8,71 @@ function getAllCars() {
         const cardElements = carsArr.map(car =>
             createCard(car)
         ).join('\n');
-        document.getElementById('cars').innerHTML = cardElements;
+        carsDiv.innerHTML = cardElements;
     });
 }
 
-function createCard(c) {
+function createCard(car) {
     return `
-    <div class="card">
-                <div class="card-header row d-flex justify-content-center" id="heading${c.id}">
+    <div class="card" id="card${car.id}">
+                <div class="card-header row d-flex justify-content-center" id="heading${car.id}">
                     <div class="col-2">
-                        <button type="button" class="btn btn-outline-danger btn-sm" onclick="deleteCar(${c.id})">Delete</button>
+                        <button type="button" class="btn btn-outline-danger btn-sm" onclick="deleteCar(${car.id})">Delete</button>
                     </div>
                     <div class="col-10 text-center">
                         <h2 class="mb-0">
                             <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
-                                data-target="#collapse${c.id}" aria-expanded="false" aria-controls="collapse${c.id}">
-                                ${c.id} ${c.mark} ${c.model} ${c.color}
+                                data-target="#collapse${car.id}" aria-expanded="false" aria-controls="collapse${car.id}">
+                                <span id="cardCarId${car.id}">${car.id}</span>  <span id="cardCarMark${car.id}">${car.mark}</span> <span id="cardCarModel${car.id}">${car.model}</span>  <span id="cardCarColor${car.id}">${car.color}</span>
                             </button>
                         </h2>
                     </div>
                 </div>
-                <div id="collapse${c.id}" class="collapse" aria-labelledby="heading${c.id}"
+                <div id="collapse${car.id}" class="collapse" aria-labelledby="heading${car.id}"
                     data-parent="#cars">
                     <div class="card-body">
-                        ${getEditMenu(c.id)}
+                        ${getEditMenu(car)}
                     </div>
                 </div>
     </div>
     `
 }
 
-function getEditMenu(id) {
-return`
+function getEditMenu(car) {
+    return `
 <form>
 <div class="form-group">
-    <label for="modelEditFormInput${id}">Model</label>
-    <input type="text" class="form-control" id="modelEditFormInput${id}">
+    <label for="markEditFormInput${car.id}">Mark</label>
+    <input required type="text" class="form-control" id="markEditFormInput${car.id}" value="${car.mark}">
 </div>
 <div class="form-group">
-    <label for="markEditFormInput${id}">Mark</label>
-    <input type="text" class="form-control" id="markEditFormInput${id}">
+    <label for="modelEditFormInput${car.id}">Model</label>
+    <input required type="text" class="form-control" id="modelEditFormInput${car.id}" value="${car.model}">
 </div>
 <div class="form-group">
-    <label for="colorEditFormInput${id}">Color</label>
-    <input type="text" class="form-control" id="colorEditFormInput${id}">
+    <label for="colorEditFormInput${car.id}">Color</label>
+    <input required type="text" class="form-control" id="colorEditFormInput${car.id}" value="${car.color}">
 </div>
+<button type="button" class="btn btn-outline-danger btn-sm" data-dismiss="modal" onclick="editCar(${car.id})">Accept edit</button>
 </form>
-<button type="button" class="btn btn-outline-danger btn-sm" data-dismiss="modal" onclick="editCar(${id})">Accept edit</button>
 
 `
 }
 
 function editCar(id) {
+    const markInputValue = document.getElementById(`markEditFormInput${id}`).value;
+    const modelInputValue = document.getElementById(`modelEditFormInput${id}`).value;
+    const colorInputValue = document.getElementById(`colorEditFormInput${id}`).value;
+
+    const newMark = (markInputValue === "") ? document.getElementById('cardCarMark' + id).innerText : markInputValue;
+    const newModel = (modelInputValue === "") ? document.getElementById('cardCarModel' + id).innerText : modelInputValue;
+    const newColor = (colorInputValue === "") ? document.getElementById('cardCarColor' + id).innerText : colorInputValue;
+
     newCar = {
         id: id,
-        mark: document.getElementById(`markEditFormInput${id}`).value,
-        model: document.getElementById(`modelEditFormInput${id}`).value,
-        color: document.getElementById(`colorEditFormInput${id}`).value
+        mark: newMark,
+        model: newModel,
+        color: newColor
     }
 
     fetch(API_URL, {
@@ -73,20 +82,36 @@ function editCar(id) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(newCar)
-    }).then(processOkResponse)
-    .catch(console.warn);
+    })
+        .then(processOkResponse)
+        .then(updateCarCard(id, newCar))
+        .catch(console.warn);
 
-    
 }
 
-function deleteCar(id){
-    fetch(`${API_URL}/${id}`, {
-    method: 'DELETE',
-    }).then(processOkResponse)
-    .catch(console.warn);
+function refreshPage() {
+    window.location.reload(true);
+}
 
-    getAllCars();
-    //todo poprawic
+function updateCarCard(id, newCar) {
+
+    const cardToUpdate = document.getElementById('card' + id);
+    cardToUpdate.innerHTML = createCard(newCar);
+
+}
+
+function deleteCar(id) {
+    fetch(`${API_URL}/${id}`, {
+        method: 'DELETE',
+    }).then(processOkResponse)
+        .then(removeCarCard(id))
+        .catch(console.warn);
+
+}
+
+function removeCarCard(id) {
+    const cardToRemove = document.getElementById('card' + id);
+    carsDiv.removeChild(cardToRemove);
 }
 
 function getParam() {
@@ -109,24 +134,19 @@ function loadCarFields() {
     }
 }
 
-function addCarModalMenu() {
-
-
+function showModalAddMenu() {
     fetch(API_URL, getParam())
         .then(processOkResponse)
         .then(createNewCar)
         .catch(console.warn);
 
-    getAllCars();  
-    //todo poprawic
 }
 
 function createNewCar() {
-    //todo moze wstawiac elementy
-
-    console.log('create new car');
-
+    let doc = new DOMParser().parseFromString(createCard(loadCarFields()), 'text/html');
+    carsDiv.append((doc.body.firstChild));
 }
+
 function processOkResponse(response = {}) {
     if (response.ok) {
         console.log(response);
